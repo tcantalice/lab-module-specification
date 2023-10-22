acc67f96bc3cdcfe5059db046b86920e4c4af1a5
Tarcisio Cantalice <tarcisiocantalice@gmail.com>
2023-10-22 18:04:08 -0300

    Cria estrutura de metadados dos módulos

---
+++ lib/Module/Contracts/Metadata.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Lab\Specification\Module\Contracts;
+
+/**
+ * Contrato dos metadados do módulo
+ *
+ * @version 1.0
+ * @author Tarcisio Cantalice <tarcisiocantalice@gmail.com>
+ */
+interface Metadata
+{
+    /**
+     * Identificação única do módulo. Cada módulo deve possuir essa identificação que
+     * irá diferenciar dos demais durante a instalação.
+     *
+     * @return string
+     */
+    public function getIdentifier(): string;
+
+    /**
+     * @return string
+     */
+    public function getVersion(): string;
+
+    /**
+     * @return string
+     */
+    public function getName(): string;
+}
